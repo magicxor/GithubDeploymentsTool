29bca38121f479d5f878ac0aeafca02b3a2cb916
Ilia Burakov <burakov.ilia@gmail.com>
2024-03-25 22:39:05 -0400

    add error logging

--- GithubDeploymentsTool/Services/Worker.cs
+++ GithubDeploymentsTool/Services/Worker.cs
@@ -30,10 +30,17 @@ public sealed class Worker
         var response = await _githubClient.RepositoryDeployments
             .ExecuteAsync(args.Owner, args.Repository, true, [args.Environment], cancellationToken);
 
+        if (response.IsErrorResult())
+        {
+            _logger.LogError("Error creating deployment. Errors: {Errors}",
+                JsonSerializer.Serialize(response.Errors, JsonSerializerOptions));
+        }
+
         _logger.LogInformation("Repository deployments found. RepositoryId: {RepositoryId}, DeploymentsCount: {DeploymentsCount}", response.Data?.Repository?.Id, response.Data?.Repository?.Deployments.Edges?.Count);
         _logger.LogInformation("Deployments list: {Deployments}", JsonSerializer.Serialize(response.Data?.Repository?.Deployments.Edges, JsonSerializerOptions));
     }
 
+    // https://docs.github.com/en/graphql/reference/mutations#createdeployment
     public async Task CreateDeploymentAsync(CancellationToken cancellationToken = default)
     {
         var args = _githubDeploymentsToolOptions.Value.Create ?? throw new Exception($"{nameof(AppOptions)}.{nameof(AppOptions.Create)} not set");
@@ -43,13 +50,19 @@ public sealed class Worker
         var repositoryResponse = await _githubClient.Repository
             .ExecuteAsync(args.Owner, args.Repository, true, cancellationToken);
 
+        if (repositoryResponse.IsErrorResult())
+        {
+            _logger.LogError("Error creating deployment. Errors: {Errors}",
+                JsonSerializer.Serialize(repositoryResponse.Errors, JsonSerializerOptions));
+        }
+
         _logger.LogInformation("Repository found. Id: {RepositoryId}", repositoryResponse.Data?.Repository?.Id);
 
         var repositoryId = repositoryResponse.Data?.Repository?.Id;
         if (repositoryId == null)
             throw new Exception("Repository not found");
 
-        var deploymentResponse = await _githubClient.CreateRepositoryDeployment.ExecuteAsync(new CreateDeploymentInput
+        var deploymentResponse = await _githubClient.CreateDeployment.ExecuteAsync(new CreateDeploymentInput
         {
             Environment = args.Environment,
             Description = args.Description,
@@ -59,13 +72,10 @@ public sealed class Worker
             RepositoryId = repositoryId,
         }, cancellationToken);
 
-        var isErrorResult = deploymentResponse.IsErrorResult();
-        var errors = deploymentResponse.Errors;
-
-        if (isErrorResult)
+        if (deploymentResponse.IsErrorResult())
         {
             _logger.LogError("Error creating deployment. Errors: {Errors}",
-                JsonSerializer.Serialize(errors, JsonSerializerOptions));
+                JsonSerializer.Serialize(deploymentResponse.Errors, JsonSerializerOptions));
         }
         else
         {
